169143446cbe2ad1c3f660efeedbf14dfaeb31a1
AlyaaElshaffei <Alyaaelshaffeimansour@gmail.com>
2024-07-10 21:28:41 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,35 @@
 import 'dart:io';
-void main(){
-stdout.write("Enter The number of test cases :");
-int n=int.parse(stdin.readLineSync()!);
-List string=[];
-for(int i=0; i<n;i++){
-  stdout.write("Enter The String :");
-  String s= stdin.readLineSync()!;
-  string.add(s);
-}
-for(int i=0; i<n;i++){
- String test= string[i];
-  if(test.contains("010")|| test.contains("101")){
-    print("$test is Good");
+void main() {
+  stdout.write("Enter the number of test cases: ");
+  int n = int.parse(stdin.readLineSync()!);
+  List<String> strings = [];
+
+  for (int i = 0; i < n; i++) {
+    String s = '';
+    do {
+      stdout.write("Enter the string (containing only '0' or '1'): ");
+      s = stdin.readLineSync()!;
+    } while (!isValidBinaryStringAlt(s));
+
+    strings.add(s);
   }
-  else{
-    print("$test is Bad");
+
+  for (int i = 0; i < n; i++) {
+    String test = strings[i];
+
+    if (test.contains("010") || test.contains("101")) {
+      print("$test is Good");
+    } else {
+      print("$test is Bad");
+    }
   }
 }
-   // String s=stdin.r
+
+bool isValidBinaryStringAlt(String s) {
+  for (var char in s.split('')) {
+    if (char != '0' && char != '1') {
+      return false;
+    }
+  }
+  return true;
 }
