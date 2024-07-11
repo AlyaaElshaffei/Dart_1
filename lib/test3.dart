06e6f8f1a3de8a1662c83a8acc7bbb19967a426e
AlyaaElshaffei <Alyaaelshaffeimansour@gmail.com>
2024-07-11 20:52:53 +0300

    test3.dart

--- lib/test3.dart
+++ lib/test3.dart
@@ -3,9 +3,26 @@ import 'dart:io';
 void main() {
   print("Enter TheNumber Of Elements Of Arrays: ");
   int n = int.parse(stdin.readLineSync()!);
-  print("Enter The Elements Of The Arrays: ");
-  List<int> a = stdin.readLineSync()!.split(' ').map(int.parse).toList();
-  List<int> b = stdin.readLineSync()!.split(' ').map(int.parse).toList();
+  print("Enter The Elements Of The Frist Array: ");
+  List<int> a = []; 
+  List<int> b = [];
+  String A= stdin.readLineSync()!;
+ a= A.split(' ').map(int.parse).toList();
+while(a.length!=n){
+  print ("Number Of Elements Must Equal $n");
+ A =stdin.readLineSync()!;
+  a= A.split(' ').map(int.parse).toList();
+}
+  print("Enter The Elements Of The Second Array: ");
+  String B= stdin.readLineSync()!;
+ b= B.split(' ').map(int.parse).toList();
+
+while(b.length!=n){
+  print ("Number Of Elements Must Equal $n");
+ B =stdin.readLineSync()!;
+  b= B.split(' ').map(int.parse).toList();
+}
+
 
   List<int> c = concatenateArrays(a, b);
 
